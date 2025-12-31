b06d1e441f4a118a69084c35a35b8329dfbd93bb
Spencer Pope <spenpo@spenpo.com>
2025-12-31 12:09:29 +0800

    remove ignore script so all migrations run

--- migrations/19700101-001-seed-legacy-migrations.sql
+++
@@ -1,9 +0,0 @@
--- Migration: seed-legacy-migrations
--- Date: 2025-12-12
--- Sequence: 001
-
--- Add your SQL migration code here
-
-INSERT INTO `wp_tigr_migrations` (`name`) VALUES
-('19700102-tiger_grades.sql'),
-('19700103-pages.sql');
